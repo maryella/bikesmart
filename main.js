1ceddebbf967faad18cd3072096b2ea23ef92203
Mary Ella Hill <hillmaryella@gmail.com>
2019-10-01 19:46:57 -0400

    added more JS to call description text and image (if present) separately

--- main.js
+++ main.js
@@ -12,25 +12,25 @@ const inputForm = document.querySelector("#inputForm")
 const formButton = inputForm.querySelector("#formButton")
 const infoHolder = document.querySelector("#infoHolder")
 
-// formButton.addEventListener("click", function(event){
-//     event.preventDefault();
-//     const cityName = cityForm.querySelector("#city").value;
-//     printWeather(cityName);
-// })
+formButton.addEventListener("click", function(event){
+    event.preventDefault();
+    const cityName = cityForm.querySelector("#city").value;
+    printWeather(cityName);
+})
 
-//Make category drop down - not in use yet, just to put to screen
-function addIncidentCategoryDropDown(categoryArray){
-  const categoryList = document.createElement("select");
-  const selectWrapper = document.querySelector("#selectWrapper")
+//Make category drop down - not in use yet
+// function addIncidentCategoryDropDown(categoryArray){
+//   const categoryList = document.createElement("select");
+//   const selectWrapper = document.querySelector("#selectWrapper")
   
-  categoryArray.forEach(function(item){
-    const categoryOption = document.createElement("option")
-    categoryOption.text = item;
-    categoryOption.value = item;
-    categoryList.append(categoryOption)
-  })
-  selectWrapper.append(categoryList)
-}
+//   categoryArray.forEach(function(item){
+//     const categoryOption = document.createElement("option")
+//     categoryOption.text = item;
+//     categoryOption.value = item;
+//     categoryList.append(categoryOption)
+//   })
+//   selectWrapper.append(categoryList)
+// }
 
 //function to get different element for each incident
 function getTitle(response){
@@ -44,13 +44,31 @@ function getTitle(response){
 function getDescription(response){
   const description = document.createElement("p")
   description.setAttribute("class", "description")
-  
-  const descriptionInfo = response.features[i]['properties']['description']
-  description.innerHTML += descriptionInfo
+  let descriptionInfo = response.features[i]['properties']['description']
+  //remove image from text
+  descriptionInfo = descriptionInfo.replace(/<img .*?>/g,"")
+  //remove links
+  descriptionInfo = descriptionInfo.replace(/<a [^>]+>[^<]*<\/a>/, '');
+  //push words only into description element
+  description.innerHTML += descriptionInfo;
   return description
-  
 }
 
+function getImage(response){
+  const description = document.createElement("div")
+  let descriptionInfo = response.features[i]['properties']['description']
+  description.innerHTML += descriptionInfo;
+  var imgTag = description.querySelector('img');
+  if (imgTag != null){
+    const sourceURL = imgTag.getAttribute('src');
+    const photo = document.createElement("img")
+    photo.setAttribute("class","photo")
+    photo.src = sourceURL
+    return photo
+    }
+}
+  
+//Do we want to add a function to add link back in?
 
 function getCoords(response){
   const coords = response.features[i]['geometry']['coordinates']
@@ -72,12 +90,20 @@ function getIncidentInfo(location, radius){ //may add incident type later
             incidentInfo.append(title)
             const description = getDescription(response)
             incidentInfo.append(description)
-      
+            const image = getImage(response)
+              if (image != undefined) {
+                incidentInfo.append(image)
+              }
+            
             //add incident info div to page
             infoHolder.append(incidentInfo)
 
             //call coords function - hopefull to use for mapping function
-            coords = getCoords(response)
+            const coords = getCoords(response)
+            const lon = coords[0]
+            const lat = coords[1]
+          
+            
           }
           
         })
