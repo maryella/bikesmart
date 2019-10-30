7d4e3bc4847a15f1d8e44bc18b16e5c0b6d0b85b
Mary Ella Hill <hillmaryella@gmail.com>
2019-10-30 12:05:37 -0400

    Last Edits

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 //get function for calling API
-function get(url){
+function get(url) {
     return fetch(url)
-        .then(function(response){
-           return response.json();
-          })
-        .then(function(data){
-          return data;
-          })
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+            return data;
+        })
 }
 
 //link to HTML
@@ -15,7 +15,7 @@ const formButton = inputForm.querySelector("#formButton")
 const infoHolder = document.querySelector("#infoHolder")
 
 //form event listener
-formButton.addEventListener("click", function(event){
+formButton.addEventListener("click", function(event) {
     event.preventDefault();
     const inputLocation = inputForm.querySelector("#location").value;
     const inputRadius = inputForm.querySelector("#radius").value;
@@ -24,93 +24,97 @@ formButton.addEventListener("click", function(event){
 })
 
 //Make category drop down: function & call function
-function addIncidentCategoryDropDown(categoryArray){
-  const categoryList = document.createElement("select");
-  const selectWrapper = document.querySelector("#selectWrapper")
-  categoryArray.forEach(function(item){
-    const categoryOption = document.createElement("option")
-    categoryOption.text = item;
-    if (item == "all"){
-      categoryOption.value = "";
-    }
-    else {
-      categoryOption.value = `incident_type=${item}`;
-    }
-    
-    categoryList.append(categoryOption)
-  })
-  selectWrapper.append(categoryList)
+function addIncidentCategoryDropDown(categoryArray) {
+    const categoryList = document.createElement("select");
+    const selectWrapper = document.querySelector("#selectWrapper")
+    categoryArray.forEach(function(item) {
+        const categoryOption = document.createElement("option")
+        categoryOption.text = item;
+        if (item == "all") {
+            categoryOption.value = "";
+        } else {
+            categoryOption.value = `incident_type=${item}`;
+        }
+
+        categoryList.append(categoryOption)
+    })
+    selectWrapper.append(categoryList)
 }
-const incidentCategories = ["all", "crash", "hazard", "theft", "unconfirmed", "infrastructure_issue",   "chop_shop"]
+const incidentCategories = ["all", "crash", "hazard", "theft", "unconfirmed", "infrastructure_issue", "chop_shop"]
 addIncidentCategoryDropDown(incidentCategories)
 
 //functions to get different element for each incident
-function getTitle(response){
-  const title = document.createElement("h4")
-  const linebar = document.createElement("hr")
-  const titleInfo = response.features[i]['properties']['title']
-  title.innerText = titleInfo
-  title.append(linebar)
-  title.setAttribute("class", "title")
-  return title
+function getTitle(response) {
+    const title = document.createElement("h4")
+    const linebar = document.createElement("hr")
+    const titleInfo = response.features[i]['properties']['title']
+
+    title.innerText = titleInfo
+    title.append(linebar)
+    title.setAttribute("class", "title")
+    title.setAttribute("id", titleInfo)
+    return title
 }
 
-function getDescription(response){
-  const description = document.createElement("p")
-  description.setAttribute("class", "description")
-  let descriptionInfo = response.features[i]['properties']['description']
-  //remove image from text
-  descriptionInfo = descriptionInfo.replace(/<img .*?>/g,"")
-  //remove links
-  descriptionInfo = descriptionInfo.replace(/<a [^>]+>[^<]*<\/a>/, '');
-  //push words only into description element
-  description.innerHTML += descriptionInfo;
-  return description
+function getDescription(response) {
+    const description = document.createElement("p")
+    description.setAttribute("class", "description")
+    let descriptionInfo = response.features[i]['properties']['description']
+        //remove image from text
+    descriptionInfo = descriptionInfo.replace(/<img .*?>/g, "")
+        //remove links
+    descriptionInfo = descriptionInfo.replace(/<a [^>]+>[^<]*<\/a>/, '');
+    //push words only into description element
+    description.innerHTML += descriptionInfo;
+    return description
 }
 
-function getImage(response){
-  const description = document.createElement("div")
-  let descriptionInfo = response.features[i]['properties']['description']
-  description.innerHTML += descriptionInfo;
-  var imgTag = description.querySelector('img');
-  if (imgTag != null){
-    const sourceURL = imgTag.getAttribute('src');
-    const photo = document.createElement("img")
-    photo.setAttribute("class","photo")
-    photo.src = sourceURL
-    return photo
+function getImage(response) {
+    const description = document.createElement("div")
+    let descriptionInfo = response.features[i]['properties']['description']
+    description.innerHTML += descriptionInfo;
+    var imgTag = description.querySelector('img');
+    if (imgTag != null) {
+        const sourceURL = imgTag.getAttribute('src');
+        const photo = document.createElement("img")
+        photo.setAttribute("class", "photo")
+        photo.src = sourceURL
+        return photo
     }
 }
-  
 
 
-function getIncidentInfo(location, radius, type){ 
+
+function getIncidentInfo(location, radius, type) {
     infoHolder.innerHTML = ""
     get(`https://bikewise.org:443/api/v2/locations/markers?${type}&proximity=${location}&proximity_square=${radius}&all=false`)
-        .then(function(response){
-        //for loop to iterate through the items
-          for (i = 0; i < response.features.length; i++) {
-            //create div to hold info for each incident
-            const incidentInfo = document.createElement("div")
-            //create each element and add it to info div
-            const title = getTitle(response)
-            incidentInfo.append(title)
-            const description = getDescription(response)
-            incidentInfo.append(description)
-            const image = getImage(response)
-              if (image != undefined) {
-                incidentInfo.append(image)
-              }
-            //add incident info div to page
-            infoHolder.append(incidentInfo)
-            //add marker to map
-            createMarker(response)
-            //recenter map
-            updateMap(response)          
-          }
+        .then(function(response) {
+            //for loop to iterate through the items
+            for (i = 0; i < response.features.length; i++) {
+                //create div to hold info for each incident
+                const incidentInfo = document.createElement("div")
+                    //create each element and add it to info div
+                const title = getTitle(response)
+                incidentInfo.append(title)
+                const description = getDescription(response)
+                incidentInfo.append(description)
+                const image = getImage(response)
+                if (image != undefined) {
+                    incidentInfo.append(image)
+                }
+                //add incident info div to page
+                infoHolder.append(incidentInfo)
+                    //add marker to map
+                createMarker(response)
+                    //recenter map
+                updateMap(response)
+            }
         })
-  
-    }
+
+}
+
+
+getIncidentInfo("Atlanta", "10", "all")
 
 
 
@@ -128,6 +132,5 @@ function getIncidentInfo(location, radius, type){
 
 //create IMMEDIATELY INVOKED FUNCTION EXPRESSION (IIFE)
 // (function(){
-    
-// })()
 
+// })()
--- map.js
+++ map.js
@@ -1,7 +1,6 @@
-
 var map;
 
-function createMarker(response){
+function createMarker(response) {
     var bikeLayer = new google.maps.BicyclingLayer();
     bikeLayer.setMap(map);
     let marker;
@@ -12,62 +11,75 @@ function createMarker(response){
         icon: 'img/bikego.svg'
     });
     marker.setMap(map);
-  }
-  // if (navigator.geolocation) {
-  //   navigator.geolocation.getCurrentPosition(function (position) {
-  //       initialLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-  //       map.setCenter(initialLocation);
-  //   });
-  // }
 
-function updateMap(response){	
-    let latlngbounds = new google.maps.LatLngBounds();	
-    let coords = response.features[i]['geometry']['coordinates'];	
-    position = new google.maps.LatLng(coords[1], coords[0])	
-    latlngbounds.extend(position);	
-    map.setCenter(latlngbounds.getCenter());	
+    const titleInfo = response.features[i]['properties']['title']
+    const shortTitle = titleInfo.substr(0, 15) + "...";
+    const titleLink = `<a href='#${titleInfo}'>${shortTitle}</a>`
+    let popUpInfo = new google.maps.InfoWindow({
+        content: titleLink
+    });
+    marker.addListener('click', function() {
+        popUpInfo.open(map, marker);
+    })
+
+
+
+}
+// if (navigator.geolocation) {
+//   navigator.geolocation.getCurrentPosition(function (position) {
+//       initialLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+//       map.setCenter(initialLocation);
+//   });
+// }
+
+function updateMap(response) {
+    let latlngbounds = new google.maps.LatLngBounds();
+    let coords = response.features[i]['geometry']['coordinates'];
+    position = new google.maps.LatLng(coords[1], coords[0])
+    latlngbounds.extend(position);
+    map.setCenter(latlngbounds.getCenter());
     //map.fitBounds(latlngbounds);	
-    }
-  
-  function initMap() {
-    var myLatLng = {lat:  33.7490, lng:-84.3880 };
-     map = new google.maps.Map(document.getElementById('map'), {
-            zoom: 10,
-            center: myLatLng,
+}
+
+function initMap() {
+    var myLatLng = { lat: 33.7490, lng: -84.3880 };
+    map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 10,
+        center: myLatLng,
     });
     let infoWindow = new google.maps.InfoWindow;
     if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(function(position) {
-        var pos = {
-          lat: position.coords.latitude,
-          lng: position.coords.longitude
-        };
-  
-        infoWindow.setPosition(pos);
-        infoWindow.setContent('Your location.');
-        infoWindow.open(map);
-        userMarker = new google.maps.Marker({	
-                  position: new google.maps.LatLng(pos),	
-                  map: map,	
-                  icon: {path: google.maps.SymbolPath.CIRCLE,	
-                  scale: 3}
-                  })
-        map.setCenter(pos);
-      }, function() {
-        handleLocationError(true, infoWindow, map.getCenter());
-      });
+        navigator.geolocation.getCurrentPosition(function(position) {
+            var pos = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            };
+
+            infoWindow.setPosition(pos);
+            infoWindow.setContent('Your location.');
+            infoWindow.open(map);
+            userMarker = new google.maps.Marker({
+                position: new google.maps.LatLng(pos),
+                map: map,
+                icon: {
+                    path: google.maps.SymbolPath.CIRCLE,
+                    scale: 3
+                }
+            })
+            map.setCenter(pos);
+        }, function() {
+            handleLocationError(true, infoWindow, map.getCenter());
+        });
     } else {
-      // Browser doesn't support Geolocation
-      handleLocationError(false, infoWindow, map.getCenter());
+        // Browser doesn't support Geolocation
+        handleLocationError(false, infoWindow, map.getCenter());
     }
-  }
-  
-  function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+}
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
     infoWindow.setPosition(pos);
     infoWindow.setContent(browserHasGeolocation ?
-                          'Error: The Geolocation service failed.' :
-                          'Error: Your browser doesn\'t support geolocation.');
+        'Error: The Geolocation service failed.' :
+        'Error: Your browser doesn\'t support geolocation.');
     infoWindow.open(map);
-  }
-  
-  
+}
